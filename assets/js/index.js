84894464239f340b3dc11145caea821be41bf3fa
Nowfelsaif01 <saifahmed020401@gmail.com>
2023-12-20 21:03:48 +0530

    all-done

--- assets/js/index.js
+++ assets/js/index.js
@@ -15,3 +15,25 @@ updateDateTime();
 
 // Set up an interval to update the date and time every minute (you can adjust the interval as needed)
 setInterval(updateDateTime, 60000);
+
+
+
+  // Function to update time
+  function updateTime() {
+    const currentTime = new Date();
+    const hours = currentTime.getHours();
+    const minutes = currentTime.getMinutes();
+
+    const timeElement = document.querySelector('.time');
+
+    // Formatting the time with hours and minutes
+    const formattedTime = `${hours}:${minutes < 10 ? '0' + minutes : minutes}`;
+
+    // Update time in the element
+    timeElement.textContent = formattedTime;
+  }
+
+  // Update time immediately and then every minute
+  updateTime(); // Call the function immediately to avoid delay on initial load
+  setInterval(updateTime, 60000); // Update time every minute (60,000 milliseconds = 1 minute)
+
